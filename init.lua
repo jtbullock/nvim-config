d899b00abdd9e91287e2aff6d87aad82c842ac0e
Josh Bullock <mail@joshb.me>
2024-04-12 22:26:46 -0400

    Complete breaking config into files

--- init.lua
+++ init.lua
@@ -20,15 +20,3 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-t>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = {"lua", "javascript"},
-	highlight = { enable = true },
-	indent = { enable = true }
-})
-vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
